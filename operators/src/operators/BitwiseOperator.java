0c6c13fd818896447bc886092f1398b0eab5d531
Rishabh <ritik1488@gmail.com>
2020-10-25 10:15:48 -0700

    7th commit

---
+++ operators/src/operators/BitwiseOperator.java
@@ -0,0 +1,37 @@
+package operators;
+import java.util.Scanner;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter value of x:  ");
+				int x=sc.nextInt();
+				System.out.println("Enter value of y:  ");
+				int y=sc.nextInt();
+				
+				System.out.println(a!=b);
+				System.out.println(a==b);
+				System.out.println(a>b);
+				
+		/* int c=a&b;
+		System.out.println(c);
+		System.out.println(a|b);
+		
+		int d= b>>1;
+		System.out.println(d);
+		
+		System.out.println(a<<1);   
+		
+		a+=2;  //a=a+2  ASSIGNMENT OPERATION
+	    System.out.println(a);
+	    
+	    System.out.println(a!=b);*/
+	    
+
+	}
+
+}
---
+++ operators/src/operators/arithematicOperation.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class arithematicOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=4;
+		int b=6;
+		float c=(float)a/(float)b;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(c);
+		
+
+	}
+
+}
